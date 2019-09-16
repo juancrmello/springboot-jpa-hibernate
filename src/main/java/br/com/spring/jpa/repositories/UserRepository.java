954af26b0500ea08655ca2afbc64c7a575c7ab9e
Juan Mello <juanribeirofx@gmail.com>
2019-09-16 01:33:45 -0300

    data injection

---
+++ src/main/java/br/com/spring/jpa/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.spring.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.spring.jpa.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
