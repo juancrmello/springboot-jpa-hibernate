6526ea59c25dd92d6c4213f0cfef283300ff6d2e
juancrmello <juanribeirofx@gmail.com>
2019-10-11 23:50:03 -0300

    queries-fujioka

--- src/main/java/br/com/spring/jpa/repositories/ProductRepository.java
+++ src/main/java/br/com/spring/jpa/repositories/ProductRepository.java
@@ -1,12 +1,26 @@
 package br.com.spring.jpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.spring.jpa.entities.Product;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	@Query("SELECT p FROM Product p WHERE p.id = :id")
+	public Product buscarPorId(Long id);
+
+	@Query("SELECT p FROM Product p WHERE p.name = :name")
+	public List<Product> buscarProdutoPorNome(@Param("name") String name);
 	
+	@Query("SELECT p FROM Product p Where p.name like %:string%")
+	public List<Product> buscarProdutoPorSubstring(String string);
+	
+	@Query("SELECT p FROM Product p WHERE description like '%Dell%'")
+		Product buscarProdutosDell();
 	
 }
--- src/main/java/br/com/spring/jpa/repositories/UserRepository.java
+++ src/main/java/br/com/spring/jpa/repositories/UserRepository.java
@@ -1,12 +1,31 @@
 package br.com.spring.jpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.spring.jpa.entities.User;
 
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	@Query("SELECT u FROM User u WHERE u.firstName = :firstName")
+	public List<User> buscarUsuarioPorPrimeiroNome(String firstName);
 	
+	@Query("SELECT u FROM User u Where u.firstName like %:string%")
+	public List<User> buscarUsuarioPorSubstringPrimeiroNome(String string);
+	
+	@Query("SELECT u FROM User u WHERE u.lastName = :lastName")
+	public List<User> buscarUsuarioPorSobreNome(String lastName);
+	
+	@Query("SELECT u FROM User u Where u.lastName like %:string%")
+		List<User> buscarUsuarioPorSubstringSobreNome(String string);
+	
+	@Query("SELECT u FROM User u WHERE u.password = :password")
+		List<User> buscarUsuarioPorSenha(String password);
+	
+	 @Query("select u from User u where u.firstName like '%Juan%'")
+	    List<User> procurar();
 	
 }
